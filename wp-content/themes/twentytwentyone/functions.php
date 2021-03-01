5752ef05ad5d9989d08f4a5af040b2cdf778e118
Muneer <muneeribnabdulsamad@gmail.com>
2021-03-01 10:38:10 +0530

    - removed residual files - added new color, changed purple color variable

--- wp-content/themes/twentytwentyone/functions.php
+++ wp-content/themes/twentytwentyone/functions.php
@@ -205,7 +205,7 @@ if ( ! function_exists( 'twenty_twenty_one_setup' ) ) {
 		$gray      = '#39414D';
 		$green     = '#D1E4DD';
 		$blue      = '#D1DFE4';
-		$purple    = '#D1D1E4';
+		$purple-500    = '#D1D1E4';
 		$red       = '#E4D1D1';
 		$orange    = '#E4DAD1';
 		$yellow    = '#EEEADD';
@@ -242,7 +242,7 @@ if ( ! function_exists( 'twenty_twenty_one_setup' ) ) {
 				array(
 					'name'  => esc_html__( 'Purple', 'twentytwentyone' ),
 					'slug'  => 'purple',
-					'color' => $purple,
+					'color' => $purple-500,
 				),
 				array(
 					'name'  => esc_html__( 'Red', 'twentytwentyone' ),
@@ -272,12 +272,12 @@ if ( ! function_exists( 'twenty_twenty_one_setup' ) ) {
 			array(
 				array(
 					'name'     => esc_html__( 'Purple to yellow', 'twentytwentyone' ),
-					'gradient' => 'linear-gradient(160deg, ' . $purple . ' 0%, ' . $yellow . ' 100%)',
+					'gradient' => 'linear-gradient(160deg, ' . $purple-500 . ' 0%, ' . $yellow . ' 100%)',
 					'slug'     => 'purple-to-yellow',
 				),
 				array(
 					'name'     => esc_html__( 'Yellow to purple', 'twentytwentyone' ),
-					'gradient' => 'linear-gradient(160deg, ' . $yellow . ' 0%, ' . $purple . ' 100%)',
+					'gradient' => 'linear-gradient(160deg, ' . $yellow . ' 0%, ' . $purple-500 . ' 100%)',
 					'slug'     => 'yellow-to-purple',
 				),
 				array(
@@ -302,12 +302,12 @@ if ( ! function_exists( 'twenty_twenty_one_setup' ) ) {
 				),
 				array(
 					'name'     => esc_html__( 'Purple to red', 'twentytwentyone' ),
-					'gradient' => 'linear-gradient(160deg, ' . $purple . ' 0%, ' . $red . ' 100%)',
+					'gradient' => 'linear-gradient(160deg, ' . $purple-500 . ' 0%, ' . $red . ' 100%)',
 					'slug'     => 'purple-to-red',
 				),
 				array(
 					'name'     => esc_html__( 'Red to purple', 'twentytwentyone' ),
-					'gradient' => 'linear-gradient(160deg, ' . $red . ' 0%, ' . $purple . ' 100%)',
+					'gradient' => 'linear-gradient(160deg, ' . $red . ' 0%, ' . $purple-500 . ' 100%)',
 					'slug'     => 'red-to-purple',
 				),
 			)
